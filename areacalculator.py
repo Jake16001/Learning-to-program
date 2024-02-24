07f5172c67840ea3406e413a0d4a2c5cfc1d5569
Jake16001 <160814225+Jake16001@users.noreply.github.com>
2024-02-23 23:07:37 -0600

    Create areacalculator.py

---
+++ areacalculator.py
@@ -0,0 +1,5 @@
+# made to experiment with floats and math
+width = float(input("Please enter the width of the rectangle: "))
+height = float(input("Please enter the height of the rectangle: "))
+print("The area of your rectangle is: " + str(width * height))
+print("The perimeter of your rectangle is: " + str(width + height))
