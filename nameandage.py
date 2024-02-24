1213f1cac988d9e305052f3278e8cd2cec9900e0
Jake16001 <160814225+Jake16001@users.noreply.github.com>
2024-02-23 23:07:02 -0600

    Create nameandage.py

---
+++ nameandage.py
@@ -0,0 +1,6 @@
+# program originally from automate the boring stuff for python, but I tweaked it
+name = str(input("What is your name?: "))
+print("It is nice to meet you " + str(name) + ".")
+print("Your name is " + str(len(name)) + " characters long.")
+age = str(input("How old are you?: "))
+print("You will be " + str(int(age) + 1) + " next year.")
