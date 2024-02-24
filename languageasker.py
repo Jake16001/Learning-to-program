7724bc919a3ecb4fa94688a827c1b434dc43cc8e
Jake16001 <160814225+Jake16001@users.noreply.github.com>
2024-02-23 23:09:01 -0600

    Create languageasker.py
    First thing I made by myself, so it's simple and doesn't really do anything beneficial

---
+++ languageasker.py
@@ -0,0 +1,12 @@
+# first thing completely created on my own
+language = str(input("Can you speak English, Chinese, or French?: "))
+unit = input("Which one?: ")
+
+if unit.upper() == "ENGLISH" or unit.upper() == "E":
+    print("You speak English? How boring! Go learn a new language you scallywag!")
+elif unit.upper() == "CHINESE" or unit.upper() == "C":
+    print("Chinese is a cool language. Especially the characters; it's like an art!")
+elif unit.upper() == "FRENCH" or unit.upper() == "F":
+    print("French is a cool language. It sounds fancy!")
+elif unit.upper() == "ALL" or unit.upper() == "A":
+    print("Dang, you can speak all!? That's quite impressive!")
