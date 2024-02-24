62402412ff059d0c180dce4d3a49c5a86bc38412
Jake16001 <160814225+Jake16001@users.noreply.github.com>
2024-02-23 23:06:24 -0600

    Create heightconverter.py

---
+++ heightconverter.py
@@ -0,0 +1,10 @@
+# one of the second things learned about python in the tutorial
+height = float(input("What is your height: "))
+unit = input("Is this in inches (in) or centimeters (cm)?: ")
+
+if unit.upper() == "INCHES" or unit.upper() == "IN" or unit.upper() == "I":
+    converted = height * 2.54
+    print("Height in centimeters (cm):" + str(converted))
+elif unit.upper() == "CENTIMETERS" or unit.upper() == "CM" or unit.upper() == "C":
+    converted = height / 2.54
+    print("Height in inches (in):" + str(converted))
