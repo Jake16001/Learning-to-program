336ae657e09b329a3ee71fc22d33505670c94cf7
Jake <160814225+Jake16001@users.noreply.github.com>
2024-02-26 12:48:57 -0600

    email&password.py

--- email&password.py
+++ email&password.py
@@ -1,9 +1,12 @@
 email = input("Enter your email: ")
 password = input("Enter your password: ")
 bold = '\033[1m'
+end = '\033[0m'
 
 while True:
-    print(bold + "You entered:")
+    print(bold + """
+    
+You entered: """ + end)
     print("Email: " + email)
     print("Password: " + password)
     print("Is this correct?")
@@ -13,6 +16,6 @@ while True:
         print("Thank you for entering your email and password.")
         break
     elif answer.upper() == "N":
-        print("Please re-enter your email.")
+        print("Please re-enter your email and password.")
         email = input("Enter your email: ")
         password = input("Enter your password: ")
