e20af778c3ccf3c7d426b8192bd8053a0fa8460c
Jake16001 <160814225+Jake16001@users.noreply.github.com>
2024-02-27 17:35:11 -0600

    Update areacalculator.py

--- areacalculator.py
+++ areacalculator.py
@@ -1,4 +1,4 @@
-# made to experiment with floats and math
+# fifth program made | by myself
 width = float(input("Please enter the width of the rectangle: "))
 height = float(input("Please enter the height of the rectangle: "))
 print("The area of your rectangle is: " + str(width * height))
